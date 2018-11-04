4ae856afd1a507ffde6d057fa79ef18f9a4907bc
Andrew <andreyshalagin@gmail.com>
2018-11-04 17:21:33 +0300

    Изменены настройки форматирования
    -установлены отступы в 2 символа ( 4 by default)

--- sandbox/src/main/java/ru/andrew/pft/sandbox/Groot.java
+++ sandbox/src/main/java/ru/andrew/pft/sandbox/Groot.java
@@ -1,7 +1,7 @@
 package ru.andrew.pft.sandbox;
 
 public class Groot {
-	public static void main(String[] args){
-		System.out.println("I am Groot!!!");
-	}
+  public static void main(String[] args) {
+    System.out.println("I am Groot!!!");
+  }
 }
