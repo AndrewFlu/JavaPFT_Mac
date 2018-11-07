46dbb88decf8aba62317024d636a96bf62102d10
Andrew <andreyshalagin@gmail.com>
2018-11-07 11:49:56 +0300

    Создан тест на класс Point
    - добавлен пакет для тестов
    - создан класс PointTest
    - в классе PointTest созданы 3 теста

---
+++ sandbox/src/test/java/ru/andrew/pft/sandbox/PointTest.java
@@ -0,0 +1,26 @@
+package ru.andrew.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PointTest {
+
+  @Test
+  public void correctDistanceCalculation(){
+    Point p1 = new Point(5, 12);
+    Assert.assertEquals(p1.pointDistance(), 7.0);
+  }
+
+  @Test
+  public void correctDistanceCalculationViaDouble(){
+    Point p2 = new Point(-1.5, 11.2);
+    Assert.assertEquals(p2.pointDistance(), 12.7);
+  }
+
+  @Test
+  public void distanceBtwPointsMustBePositive(){
+    Point p3 = new Point(-17, -42);
+    Assert.assertTrue(p3.pointDistance() >= 0);
+    Assert.assertEquals(p3.pointDistance(), 25);
+  }
+}
