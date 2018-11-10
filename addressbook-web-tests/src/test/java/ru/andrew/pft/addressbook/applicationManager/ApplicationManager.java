c7401d4e1064e9ecef5824b019e6cbf4413c3df9
Andrew <andreyshalagin@gmail.com>
2018-11-10 12:11:41 +0300

    Добавлен помощник GroupHelper

--- addressbook-web-tests/src/test/java/ru/andrew/pft/addressbook/applicationManager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/andrew/pft/addressbook/applicationManager/ApplicationManager.java
@@ -1,22 +1,27 @@
 package ru.andrew.pft.addressbook.applicationManager;
 
-import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import ru.andrew.pft.addressbook.model.GroupData;
 import ru.andrew.pft.addressbook.model.ContactData;
+
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
 
   private WebDriver driver;
+  private GroupHelper groupHelper;
 
   public void init() {
     String projectPath = System.getProperty("user.dir");
     System.setProperty("webdriver.gecko.driver", projectPath + "/drivers/Firexox/geckodriver");
 
     driver = new FirefoxDriver();
-
+    groupHelper = new GroupHelper(driver);
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
     driver.get("http://localhost/addressbook/index.php");
     login("admin", "secret");
   }
@@ -34,26 +39,6 @@ public class ApplicationManager {
     driver.findElement(By.id("LoginForm")).submit();
   }
 
-  public void submitGroupCreation() {
-    driver.findElement(By.name("submit")).click();
-  }
-
-  public void fillGroupForm(GroupData groupData) {
-    driver.findElement(By.name("group_name")).click();
-    driver.findElement(By.name("group_name")).clear();
-    driver.findElement(By.name("group_name")).sendKeys(groupData.getGroupName());
-    driver.findElement(By.name("group_header")).click();
-    driver.findElement(By.name("group_header")).clear();
-    driver.findElement(By.name("group_header")).sendKeys(groupData.getGroupHeader());
-    driver.findElement(By.name("group_footer")).click();
-    driver.findElement(By.name("group_footer")).clear();
-    driver.findElement(By.name("group_footer")).sendKeys(groupData.getGroupFooter());
-  }
-
-  public void initGroupCreation() {
-    driver.findElement(By.name("new")).click();
-  }
-
   public void gotoHomePage() {
     driver.findElement(By.linkText("home")).click();
   }
@@ -64,10 +49,6 @@ public class ApplicationManager {
 
   public void gotoGroupPage() {
     driver.findElement(By.linkText("groups")).click();
-  }
-
-  public void returnToGroupPage() {
-    driver.findElement(By.linkText("group page")).click();
   }
 
   public void submitContactCreation() {
@@ -93,14 +74,6 @@ public class ApplicationManager {
     driver.findElement(By.linkText("add new")).click();
   }
 
-  public void deleteSelectedGroups() {
-    driver.findElement(By.name("delete")).click();
-  }
-
-  public void selectGroup() {
-    driver.findElement(By.name("selected[]")).click();
-  }
-
   public boolean isElementPresent(By by) {
     try {
       driver.findElement(by);
@@ -117,5 +90,9 @@ public class ApplicationManager {
     } catch (NoAlertPresentException e) {
       return false;
     }
+  }
+
+  public GroupHelper getGroupHelper() {
+    return groupHelper;
   }
 }
--- addressbook-web-tests/src/test/java/ru/andrew/pft/addressbook/tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/andrew/pft/addressbook/tests/GroupCreationTest.java
@@ -8,9 +8,9 @@ public class GroupCreationTest extends TestBase{
   @Test
   public void testGroupCreation() {
     app.gotoGroupPage();
-    app.initGroupCreation();
-    app.fillGroupForm(new GroupData("Group1", "GroupHeader", "GroupFooter"));
-    app.submitGroupCreation();
-    app.returnToGroupPage();
+    app.getGroupHelper().initGroupCreation();
+    app.getGroupHelper().fillGroupForm(new GroupData("Group1", "GroupHeader", "GroupFooter"));
+    app.getGroupHelper().submitGroupCreation();
+    app.getGroupHelper().returnToGroupPage();
   }
 }
