02a4baf73768a3a3b15103970bcab042818ad8d7
Andrew <andreyshalagin@gmail.com>
2018-11-11 19:18:08 +0300

    Добавлен класс по расчету дискриминанта и тестовый класс для его проверки
    В модуле sandbox:
    - создан класс Equation по проверке количества решений квадртного уравнения
    - добавлен тестовый класс для проверки

---
+++ sandbox/src/test/java/ru/andrew/pft/sandbox/EquationTest.java
@@ -0,0 +1,38 @@
+package ru.andrew.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class EquationTest {
+  @Test
+  public void test0(){
+    Equation e = new Equation(2 ,2 , 1);
+    Assert.assertEquals(e.rootNumber(), 0);
+  }
+  @Test
+  public void test1(){
+    Equation e = new Equation(1 ,2 , 1);
+    Assert.assertEquals(e.rootNumber(), 1);
+  }
+  @Test
+  public void test2(){
+    Equation e = new Equation(2 ,4 , 1);
+    Assert.assertEquals(e.rootNumber(), 2);
+  }
+  @Test
+  public void testLinear(){
+    Equation e = new Equation(0 ,1, 2);
+    Assert.assertEquals(e.rootNumber(), 1);
+  }
+  @Test
+  public void testConstant(){
+    Equation e = new Equation(0 ,0 , 1);
+    Assert.assertEquals(e.rootNumber(), 0);
+  }
+  @Test
+  public void testZero(){
+    Equation e = new Equation(0 ,0 , 0);
+    Assert.assertEquals(e.rootNumber(), -1);
+  }
+
+}
