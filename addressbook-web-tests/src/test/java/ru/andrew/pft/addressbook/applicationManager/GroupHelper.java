62c460c12d299eb2d2a63ae0e2cb244a9847f6aa
Andrew <andreyshalagin@gmail.com>
2018-11-25 21:36:18 +0300

    Добавлено кэширование для тестов по группам
    - результаты считывания данных по группам теперь кэшируются
    - после создания, модификации, удаления групп - кэш сбрасывается

--- addressbook-web-tests/src/test/java/ru/andrew/pft/addressbook/applicationManager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/andrew/pft/addressbook/applicationManager/GroupHelper.java
@@ -6,11 +6,11 @@ import org.openqa.selenium.WebElement;
 import ru.andrew.pft.addressbook.model.GroupData;
 import ru.andrew.pft.addressbook.model.Groups;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class GroupHelper extends HelperBase{
+
+  private Groups cashedGroup = null;
 
   public GroupHelper(WebDriver driver) {
     super(driver);
@@ -54,6 +54,7 @@ public class GroupHelper extends HelperBase{
     initGroupCreation();
     fillGroupForm(group);
     submitGroupCreation();
+    cashedGroup = null;
     returnToGroupPage();
   }
 
@@ -62,12 +63,14 @@ public class GroupHelper extends HelperBase{
     initGroupModification();
     fillGroupForm(group);
     submitGroupModification();
+    cashedGroup = null;
     returnToGroupPage();
   }
 
   public void delete(GroupData group) {
     selectGroupById(group.getId());
     deleteSelectedGroups();
+    cashedGroup = null;
     returnToGroupPage();
   }
 
@@ -81,13 +84,16 @@ public class GroupHelper extends HelperBase{
   }
 
   public Groups all(){
+    if (cashedGroup != null){
+      return new Groups(cashedGroup);
+    }
     List<WebElement> elements = driver.findElements(By.cssSelector("span.group"));
-    Groups groupData = new Groups();
+    cashedGroup = new Groups();
     for (WebElement element : elements){
       int id = Integer.parseInt(element.findElement(By.name("selected[]")).getAttribute("value"));
       String name = element.getText();
-      groupData.add(new GroupData().withId(id).withName(name));
+      cashedGroup.add(new GroupData().withId(id).withName(name));
     }
-    return groupData;
+    return new Groups(cashedGroup);
   }
 }
