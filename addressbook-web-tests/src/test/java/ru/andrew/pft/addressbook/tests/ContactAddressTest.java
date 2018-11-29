731aeac8ad850a0d926d6527be3e0ff75f77e514
Andrew <andreyshalagin@gmail.com>
2018-11-29 22:48:06 +0300

    Правки в тест на проверку адреса
    Теперь проверка адреса выполняется с очисткой только пробелов внутри данных, полученных из формы редактирования

--- addressbook-web-tests/src/test/java/ru/andrew/pft/addressbook/tests/ContactAddressTest.java
+++ addressbook-web-tests/src/test/java/ru/andrew/pft/addressbook/tests/ContactAddressTest.java
@@ -5,6 +5,9 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.andrew.pft.addressbook.model.ContactData;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -23,10 +26,11 @@ public class ContactAddressTest extends TestBase {
     ContactData contact = app.contact().all().iterator().next();
     ContactData contactDataFromEditForm = app.contact().infoFromEditForm(contact);
 
-    assertThat(cleared(contact.getAddress()), equalTo(cleared(contactDataFromEditForm.getAddress())));
+    assertThat(contact.getAddress(), equalTo(cleared(contactDataFromEditForm.getAddress())));
   }
 
   private String cleared(String address) {
-    return address.replaceAll("\\s", "");
+    String[] split = address.split("\\n");
+    return Arrays.asList(split).stream().map(String::trim).collect(Collectors.joining("\n"));
   }
 }
