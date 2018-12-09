46a5f1e531cecadc2b7217b0d301de1b3e21c719
Andrew <andreyshalagin@gmail.com>
2018-12-09 18:01:50 +0300

    Добавлена библиотека jCommander для параметризации запсука тестов с командной строки

--- addressbook-web-tests/src/test/java/ru/andrew/pft/addressbook/Generators/GroupDataGenerators.java
+++ addressbook-web-tests/src/test/java/ru/andrew/pft/addressbook/Generators/GroupDataGenerators.java
@@ -1,5 +1,8 @@
 package ru.andrew.pft.addressbook.Generators;
 
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
 import ru.andrew.pft.addressbook.model.GroupData;
 
 import java.io.File;
@@ -11,16 +14,28 @@ import java.util.List;
 
 public class GroupDataGenerators {
 
-  public static void main(String[] args) throws IOException {
-    int count = Integer.parseInt(args[0]);
-    File file = new File(args[1]);
+  @Parameter(names = "-c", description = "Group count")
+  public int count;
 
-    List<GroupData> groups = generateGroups(count);
-    save(groups, file);
+  @Parameter(names = "-f", description = "Destination file")
+  public String file;
+
+  public static void main(String[] args) throws IOException {
+    GroupDataGenerators generators = new GroupDataGenerators();
+    try {
+      JCommander.newBuilder().addObject(generators).build().parse(args);
+    } catch (ParameterException ex) {
+      ex.usage();
+    }
+    generators.run();
   }
 
-  private static void save(List<GroupData> groups, File file) throws IOException {
+  private void run() throws IOException {
+    List<GroupData> groups = generateGroups(count);
+    save(groups, new File(file));
+  }
 
+  private void save(List<GroupData> groups, File file) throws IOException {
     Writer fileWriter = new FileWriter(file);
     for (GroupData group : groups) {
       fileWriter.write(String.format("%s;%s;%s\n", group.getGroupName(), group.getGroupHeader(), group.getGroupFooter()));
@@ -28,10 +43,11 @@ public class GroupDataGenerators {
     fileWriter.close();
   }
 
-  private static List<GroupData> generateGroups(int count) {
+  private List<GroupData> generateGroups(int count) {
     List<GroupData> groups = new ArrayList<GroupData>();
     for (int i = 0; i < count; i++) {
-      groups.add(new GroupData().withName(String.format("GroupName_%s", i))
+      groups.add(new GroupData()
+              .withName(String.format("GroupName_%s", i))
               .withHeader(String.format("GroupHeader_%s", i))
               .withFooter(String.format("GroupFooter_%s", i)));
     }
