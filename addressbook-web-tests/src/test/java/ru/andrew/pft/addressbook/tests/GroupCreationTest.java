7246fbe9a04d9ea7431caf3ea249bc2609657e3f
Andrew <andreyshalagin@gmail.com>
2018-12-10 20:47:25 +0300

    В класс DataProvider добавлено считывание данных из файла
    - параметризован тест на создание группы с пустыми header и footer

--- addressbook-web-tests/src/test/java/ru/andrew/pft/addressbook/tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/andrew/pft/addressbook/tests/GroupCreationTest.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 import ru.andrew.pft.addressbook.model.GroupData;
 import ru.andrew.pft.addressbook.model.Groups;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -15,10 +16,15 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class GroupCreationTest extends TestBase {
 
   @DataProvider
-  public Iterator<Object[]> validGroups() {
+  public Iterator<Object[]> validGroups() throws IOException {
     List<Object[]> groups = new ArrayList<Object[]>();
-    groups.add(new Object[] {new GroupData().withName("name_1").withHeader("header_1").withFooter("footer_1")});
-    groups.add(new Object[] {new GroupData().withName("name_2").withHeader("header_2").withFooter("footer_2")});
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groupData.csv")));
+    String line = reader.readLine();
+    while (line != null){
+      String[] split = line.split(";");
+      groups.add(new Object[]{new GroupData().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
+      line = reader.readLine();
+    }
     return groups.iterator();
   }
 
@@ -32,11 +38,11 @@ public class GroupCreationTest extends TestBase {
     assertThat(after, equalTo(before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
   }
 
-  @Test
-  public void testGroupCreationWithEmptyHeaderAndFooter() {
+  @Test (dataProvider = "validGroups")
+  public void testGroupCreationWithEmptyHeaderAndFooter(GroupData group) {
+    group.withFooter(null).withHeader(null);
     app.goTo().groupPage();
     Groups before = app.group().all();
-    GroupData group = new GroupData().withName("Group1");
     app.group().create(group);
     assertThat(app.group().getGroupsCount(), equalTo(before.size() + 1));
     Groups after = app.group().all();
