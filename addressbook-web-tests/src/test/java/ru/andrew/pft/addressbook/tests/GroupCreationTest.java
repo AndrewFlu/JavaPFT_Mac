419b7407642b7597e8cd0e9bd4c5269adc8fb631
Andrew <andreyshalagin@gmail.com>
2018-12-12 21:46:26 +0300

    Применена конструкция tryWithResources
    - благодаря  конструкции tryWithResources объекты типа Writer & Reader закрывать в ручную не нужно, они закроются автоматически

--- addressbook-web-tests/src/test/java/ru/andrew/pft/addressbook/tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/andrew/pft/addressbook/tests/GroupCreationTest.java
@@ -8,7 +8,10 @@ import org.testng.annotations.Test;
 import ru.andrew.pft.addressbook.model.GroupData;
 import ru.andrew.pft.addressbook.model.Groups;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -20,38 +23,37 @@ public class GroupCreationTest extends TestBase {
 
   @DataProvider
   public Iterator<Object[]> validGroupsFromXml() throws IOException {
-    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groupData.xml")));
-    String line = reader.readLine();
-    String xml = "";
-    while (line != null){
-      xml += line;
-      line = reader.readLine();
+    try (BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groupData.xml")))) {
+      String line = reader.readLine();
+      String xml = "";
+      while (line != null) {
+        xml += line;
+        line = reader.readLine();
+      }
+      XStream xStream = new XStream();
+      xStream.processAnnotations(GroupData.class);
+      List<GroupData> groupData = (List<GroupData>) xStream.fromXML(xml);
+      return groupData.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
     }
-    reader.close();
-
-    XStream xStream = new XStream();
-    xStream.processAnnotations(GroupData.class);
-    List<GroupData> groupData = (List<GroupData>) xStream.fromXML(xml);
-    return groupData.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
   }
 
   @DataProvider
   public Iterator<Object[]> validGroupsFromJson() throws IOException {
-    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groupData.json")));
-    String line = reader.readLine();
-    String json = "";
-    while (line != null){
-      json += line;
-      line = reader.readLine();
+    try (BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groupData.json")))) {
+      String line = reader.readLine();
+      String json = "";
+      while (line != null) {
+        json += line;
+        line = reader.readLine();
+      }
+      Gson gson = new Gson();
+      List<GroupData> groups = gson.fromJson(json, new TypeToken<List<GroupData>>() {
+      }.getType());
+      return groups.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
     }
-    reader.close();
-
-    Gson gson = new Gson();
-    List<GroupData> groups = gson.fromJson(json, new TypeToken<List<GroupData>>() {}.getType());
-    return groups.stream().map((g)-> new Object[] {g}).collect(Collectors.toList()).iterator();
   }
 
-  @Test (dataProvider = "validGroupsFromJson")
+  @Test(dataProvider = "validGroupsFromJson")
   public void testGroupCreation(GroupData group) {
     app.goTo().groupPage();
     Groups before = app.group().all();
@@ -61,7 +63,7 @@ public class GroupCreationTest extends TestBase {
     assertThat(after, equalTo(before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
   }
 
-  @Test (dataProvider = "validGroupsFromXml")
+  @Test(dataProvider = "validGroupsFromXml")
   public void testGroupCreationWithEmptyHeaderAndFooter(GroupData group) {
     group.withFooter(null).withHeader(null);
     app.goTo().groupPage();
