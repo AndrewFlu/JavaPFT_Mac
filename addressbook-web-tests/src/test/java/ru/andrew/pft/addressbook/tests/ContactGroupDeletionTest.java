d5cb49446cee01dc53833ed13dc2d78b1211618a
Andrew <andreyshalagin@gmail.com>
2019-01-05 16:33:32 +0300

    Добавлен тест на удаление контакта из группы

--- addressbook-web-tests/src/test/java/ru/andrew/pft/addressbook/tests/ContactGroupDeletionTest.java
+++ addressbook-web-tests/src/test/java/ru/andrew/pft/addressbook/tests/ContactGroupDeletionTest.java
@@ -7,24 +7,22 @@ import ru.andrew.pft.addressbook.model.Contacts;
 import ru.andrew.pft.addressbook.model.GroupData;
 import ru.andrew.pft.addressbook.model.Groups;
 
-import java.util.Spliterators;
-
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactGroupDeletionTest extends TestBase {
 
   @BeforeMethod
-  public void ensurePreconditions(){
-    if (app.db().groups().size() < 1){
+  public void ensurePreconditions() {
+    if (app.db().groups().size() < 1) {
       app.goTo().groupPage();
       app.group().create(new GroupData().withName("NameForGroupDeletion"));
     }
     Groups listGroups = app.db().groups();
-    if (app.db().contacts().size() == 0){
+    if (app.db().contacts().size() == 0) {
       app.goTo().homePage();
       app.contact().create(new ContactData().withName("NameForGroupDeletion").withLastName("LastNameForGroupDeletion")
-      .inGroup(listGroups.iterator().next()));
+              .inGroup(listGroups.iterator().next()));
     }
   }
 
@@ -33,7 +31,7 @@ public class ContactGroupDeletionTest extends TestBase {
     Contacts contacts = app.db().contacts();
     ContactData contact = contacts.iterator().next();
 
-    while (contact.getGroups().size() == 0){
+    while (contact.getGroups().size() == 0) {
       contacts.remove(contact);
       contact = contacts.iterator().next();
     }
