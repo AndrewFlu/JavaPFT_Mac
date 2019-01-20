d49f2114989661657a5e73b1274d354a4c72c148
Andrew <andreyshalagin@gmail.com>
2019-01-20 11:57:36 +0300

    В классе TestBase добавлены методы для загрузки настроек web-сервера
    - в метод init() добавлено выполнение метода upload();
    - в методе tearDown() добавлено выполнение метода restore()

--- mantis-tests/src/test/java/ru/andrew/mantis/tests/TestBase.java
+++ mantis-tests/src/test/java/ru/andrew/mantis/tests/TestBase.java
@@ -5,6 +5,7 @@ import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.andrew.mantis.applicationManager.ApplicationManager;
 
+import java.io.File;
 import java.io.IOException;
 
 public class TestBase {
@@ -15,10 +16,12 @@ public class TestBase {
   @BeforeSuite(alwaysRun = true)
   public void setUp() throws IOException {
     app.init();
+    app.ftp().upload(new File("src/test/resources/config_defaults_inc.php"), "config_defaults_inc.php", "config_defaults_inc.php.bak");
   }
 
   @AfterSuite(alwaysRun = true)
-  public void tearDown() {
+  public void tearDown() throws IOException {
+    app.ftp().restore("config_defaults_inc.php.bak", "config_defaults_inc.php");
     app.stop();
   }
 
