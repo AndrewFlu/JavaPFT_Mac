ae97376b66480e2b0498de3399e079fc18489a97
Andrew <andreyshalagin@gmail.com>
2019-01-26 16:26:25 +0300

    Добавлен помощник по работе с внешним почтовым сервером (James)
    - работа помощника не проверена, т.к. сраный MacBook нормально не работает ни с сервером, ни с протоколом telnet

--- mantis-tests/src/test/java/ru/andrew/mantis/applicationManager/JamesHelper.java
+++ mantis-tests/src/test/java/ru/andrew/mantis/applicationManager/JamesHelper.java
@@ -3,12 +3,14 @@ package ru.andrew.mantis.applicationManager;
 import org.apache.commons.net.telnet.TelnetClient;
 import ru.andrew.mantis.model.MailMessage;
 
-import javax.mail.Session;
+import javax.mail.*;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintStream;
 import java.net.SocketException;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class JamesHelper {
 
@@ -18,6 +20,7 @@ public class JamesHelper {
   private String mailserver;
   private InputStream inputStream;
   private PrintStream outputStream;
+  private Store store;
 
   public JamesHelper(ApplicationManager app) {
     this.app = app;
@@ -30,6 +33,14 @@ public class JamesHelper {
     write("adduser " + user + " " + password);
     readUntil("User " + user + " deleted");
     closeTelnetSession();
+  }
+
+  public void clearAllMessagesInMailBox(String username, String password) throws MessagingException {
+    Folder inbox = openInbox(username, password);
+    for (Message message : inbox. getMessages()){
+      message.setFlag(Flags.Flag.DELETED, true);
+    }
+    closeFolder(inbox);
   }
 
   private void closeTelnetSession() {
@@ -99,6 +110,51 @@ public class JamesHelper {
     return null;
   }
 
-  public List<MailMessage> waitForMail(String user, String password, int timeout) {
+  public List<MailMessage> waitForMail(String user, String password, int timeout) throws MessagingException {
+    long now = System.currentTimeMillis();
+    while (System.currentTimeMillis() < now + timeout){
+      List<MailMessage> allMail = getAllMail(user, password);
+      if (allMail.size()>0){
+        return allMail;
+      }
+      try {
+        Thread.sleep(1000);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
+    throw new Error("No mail :(");
+  }
+
+  private List<MailMessage> getAllMail(String user, String password) throws MessagingException {
+    Folder inbox = openInbox(user, password);
+    List <MailMessage> mails = Arrays.asList(inbox.getMessages()).stream().map((m) -> toModelMail(m)).collect(Collectors.toList());
+    closeFolder(inbox);
+    return  mails;
+  }
+
+  private void closeFolder(Folder folder) throws MessagingException {
+    folder.close(true);
+    store.close();
+  }
+
+  private Folder openInbox(String user, String password) throws MessagingException {
+    store = mailSession.getStore("POP3");
+    store.connect(mailserver, user, password);
+    Folder folder = store.getDefaultFolder().getFolder("INBOX");
+    folder.open(Folder.READ_WRITE);
+    return folder;
+  }
+
+  private static MailMessage toModelMail(Message m) {
+    try {
+      return new MailMessage(m.getAllRecipients()[0].toString(), (String) m.getContent());
+    } catch (MessagingException e) {
+      e.printStackTrace();
+      return null;
+    } catch (IOException e) {
+      e.printStackTrace();
+      return null;
+    }
   }
 }
