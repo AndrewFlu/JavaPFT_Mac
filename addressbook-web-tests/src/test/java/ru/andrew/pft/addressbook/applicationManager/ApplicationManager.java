013bed21d39fe7c6365912663caf651f0805c72b
Andrew <andreyshalagin@gmail.com>
2019-02-10 19:29:32 +0300

    Добавлена возможность делать скриншоты и прикреплять их к отчету
    - добавлен тестовый класс, реализующий интерфейс ITestListener
    - ссылка на Application manager передается через контекст выполнения тестов. Устанавливается в базовом тестовом классе и передается в методе init () командой context.setAttribute("app", app);
    - а используется в переопределяющем методе, для тестов завершившихся с ошибкой
    - испорчен тест testBadGroupCreation()

--- addressbook-web-tests/src/test/java/ru/andrew/pft/addressbook/applicationManager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/andrew/pft/addressbook/applicationManager/ApplicationManager.java
@@ -1,6 +1,8 @@
 package ru.andrew.pft.addressbook.applicationManager;
 
 import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -85,5 +87,9 @@ public class ApplicationManager {
 
   public DbHelper db(){
     return dbHelper;
+  }
+
+  public byte[] takeScreenshot(){
+    return ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
   }
 }
